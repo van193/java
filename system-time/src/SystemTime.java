f2d330d40306342b0b6b2ba544d69ed3567b8c0e
Yun Cmpsr <vuthibichvan101206@gmail.com>
2025-02-05 05:28:32 +0700

    commit

---
+++ system-time/src/SystemTime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
