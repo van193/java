67ea5cda2d6e19213de8996e1977781f89b9a22d
Yun Cmpsr <vuthibichvan101206@gmail.com>
2025-02-05 07:15:05 +0700

    new update

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
+        System.out.println("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Hello:" + name);
+        System.out.println("Hello: " + name);
     }
 }
