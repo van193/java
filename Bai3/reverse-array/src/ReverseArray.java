6ccfb7a5b04b73d99818f5b356bb48f02fab778f
Yun Cmpsr <vuthibichvan101206@gmail.com>
2025-02-07 03:16:50 +0700

    new update

--- Bai3/reverse-array/src/ReverseArray.java
+++ Bai3/reverse-array/src/ReverseArray.java
@@ -19,15 +19,18 @@ public class ReverseArray {
             array[i] = scanner.nextInt();
             i++;
         }
+
         System.out.printf("%-20s%s", "Elements in array: ", "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
         }
+
         for (int j = 0; j < array.length / 2; j++) {
             int temp = array[j];
             array[j] = array[size - 1 - j];
             array[size - 1 - j] = temp;
         }
+
         System.out.printf("\n%-20s%s", "Reverse array: ", "");
         for (int j = 0; j < array.length; j++) {
             System.out.print(array[j] + "\t");
