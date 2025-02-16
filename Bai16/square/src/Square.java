37395f14df410f43a362ce7458e636be3d9b31da
Yun Cmp <vuthibichvan101206@gmail.com>
2025-02-16 11:29:40 +0700

    new update

---
+++ Bai16/square/src/Square.java
@@ -0,0 +1,39 @@
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super();
+    }
+    
+    public Square(double side, String color, boolean filled) {
+        super();
+    }
+
+    public double getSide() {
+        return getWidth();
+    }
+
+    public void setSide(double side) {
+        setWidth(side);
+        setLength(side);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        setSide(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        setSide(length);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
