b22fe10dddc09e5ab2f80eff2ec6655fd1473f84
Yun Cmp <vuthibichvan101206@gmail.com>
2025-02-16 13:33:15 +0700

    new update

--- Bai1/Hello/src/Hello.java
+++ Bai1/Hello/src/Hello.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
-        String name = sc.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello: " + name);
     }
 }
