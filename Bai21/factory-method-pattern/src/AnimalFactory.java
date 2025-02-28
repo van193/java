d224927dd46fe154e4253f83268f92ae2eda3581
Yun Cmp <vuthibichvan101206@gmail.com>
2025-02-28 20:24:12 +0700

    init new

--- Bai21/factory-method-pattern/src/AnimalFactory.java
+++
@@ -1,9 +0,0 @@
-public class AnimalFactory {
-    public Animal getAnimal(String type) {
-        if ("canine".equals(type)) {
-            return new Dog();
-        } else {
-            return new Cat();
-        }
-    }
-}
